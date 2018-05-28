29198ce9f5d73b937f8b651dc91638ff9fd39c54
shantidyah <shanti.dyah@gmail.com>
2018-05-28 12:55:42 +0700

    money coins

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,16 @@
 function convertToCoin (money) {
   // your implementation code here
+  var coin = [1,10,20,50,100,200,500,1000,2000,5000,10000]
+  var arrConvert =[]
+
+  for (var i = coin.length; i >= 0; i--) {
+    if (money>=coin[i]){
+      arrConvert.push(coin[i])
+      money-=coin[i]
+      i++
+    }
+  }
+  return arrConvert;
 }
 
 // Drive code
