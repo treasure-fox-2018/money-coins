6823f6efc8ee483a31e9a7f74e771b231c1681ed
Yusuf Salman Noerinaldy Siregar <yusufsiregar5@gmail.com>
2018-05-28 15:20:29 +0700

    add roman_numerals.js

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,56 @@
 function convertToCoin (money) {
-  // your implementation code here
+  var arrOfObjConversion = [
+    {
+      number: 1,
+    },
+    {
+      number: 10,
+    },
+    {
+      number: 20,
+    },
+    {
+      number: 50,
+    },
+    {
+      number: 100,
+    },
+    {
+      number: 500,
+    },
+    {
+      number: 1000,
+    },
+    {
+      number: 2000,
+    },
+    {
+      number: 5000,
+    },
+    {
+      number: 10000,
+    }
+  ];
+  var i = arrOfObjConversion.length - 1;
+  var arrOfCoins = [];
+
+  while (money !== 0) {
+    var arrayAccessNumber = arrOfObjConversion[i].number;
+    var inspector = money - arrayAccessNumber;
+    // console.log(inspector);
+    if (inspector >= 0) {
+      money -= arrayAccessNumber;
+      arrOfCoins.push(arrayAccessNumber)
+      // console.log(tempoRomanConversion);
+      // console.log(tempoNumberSum);
+    } else if (inspector < 0) {
+      // console.log("keluar");
+      i --;
+      // console.log(arrayAccessNumber);
+      // console.log(arrayAccessRoman);
+    }
+  }
+  return arrOfCoins;
 }
 
 // Drive code
