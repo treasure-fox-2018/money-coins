f346cd82464a105c77dfdeb216f45591206b32c3
wisnugautama <ngurahwisnugautama@gmail.com>
2018-05-28 20:39:08 +0700

    exercise money coins done

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,27 @@
 function convertToCoin (money) {
   // your implementation code here
+  var newArray = [];
+  var coin = {
+    uang1: 10000,
+    uang2: 5000,
+    uang3: 2000,
+    uang4: 1000,
+    uang5: 500,
+    uang6: 200,
+    uang7: 100,
+    uang8: 50,
+    uang9: 20,
+    uang10: 10,
+    uang11: 1
+  }
+
+  for(var i in coin){
+    while (money >= coin[i]) {
+      newArray.push(coin[i]);
+      money -= coin[i]
+    }
+  }
+  return newArray;
 }
 
 // Drive code
