bd9c0c2502a0d580bedb9207dd37aa752618a309
fadhilhaka <fadhilhanri@gmail.com>
2018-05-30 13:03:32 +0700

    money coins fadhil hanri kamarz

--- money_coins.js
+++ money_coins.js
@@ -1,108 +1,30 @@
-function moneySatuan(money) {
-  var hasilSatuan = []
-  for (var a = 0; a < money; a++) {
-    hasilSatuan.push(1)
-  }
-  return hasilSatuan
-}
-
-function moneyPuluhan(money) {
-  var hasilPuluhan = []
-  while (money > 1) {
-    if (money >= 5) {
-      hasilPuluhan.push(50)
-      money = money - 5
-    }
-    if (money >= 2) {
-      hasilPuluhan.push(20)
-      money = money - 2
-    }
-  }
-  if (money === 1) {
-    hasilPuluhan.push(10)
-  }
-  return hasilPuluhan
-}
-
-function moneyRatusan(money) {
-  var hasilRatusan = []
-  while (money > 1) {
-    if (money >= 5) {
-      hasilRatusan.push(500)
-      money = money - 5
-    }
-    if (money >= 2) {
-      hasilRatusan.push(200)
-      money = money - 2
-    }
-  }
-  if (money === 1) {
-    hasilPuluhan.push(100)
-  }
-  return hasilRatusan
-}
-
-function moneyRibuan(money) {
-  var hasilRibuan = []
-  while (money > 1) {
-    if (money >= 5) {
-      hasilRibuan.push(5000)
-      money = money - 5
-    }
-    if (money >= 2) {
-      hasilRibuan.push(2000)
-      money = money - 2
-    }
-  }
-  if (money === 1) {
-    hasilRibuan.push(1000)
-  }
-  return hasilRibuan
-}
-
-function moneyPuluhRibuan(money) {
-  var hasilPuluhRibuan = []
-  for (var b = 0; b < money; b++) {
-    hasilPuluhRibuan.push(10000)
-  } 
-  return hasilPuluhRibuan
-}
-
 function convertToCoin (money) {
   // your implementation code here
-  var arrMoney = []
-  while (money >= 10) {
-    var angkaAkhir = money % 10
-    arrMoney.push(angkaAkhir)
-    money = Math.floor(money / 10)
+  let moneyObj = {
+    10000: [],
+    5000: [],
+    2000: [],
+    1000: [],
+    500: [],
+    200: [],
+    100: [],
+    50: [],
+    20: [],
+    10: [],
+    1: [],
   }
-  arrMoney.push(money)
-  var hasil = []
-  for (var c = 0; c < arrMoney.length; c++) {
-    if (c === 0) {
-      var satuan = moneySatuan(arrMoney[c])
-      hasil.unshift(satuan)
-    } else if (c === 1) {
-      var puluhan = moneyPuluhan(arrMoney[c])
-      hasil.unshift(puluhan)
-    } else if (c === 2) {
-      var ratusan = moneyRatusan(arrMoney[c])
-      hasil.unshift(ratusan)
-    } else if (c === 3) {
-      var ribuan = moneyRibuan(arrMoney[c])
-      hasil.unshift(ribuan)
-    } else if (c === 4) {
-      var puluhRibuan = moneyPuluhRibuan(arrMoney[c])
-      hasil.unshift(puluhRibuan)
+
+  let keys = Object.keys(moneyObj)
+  let hasil = []
+
+  for (let a = keys.length-1; a >= 0; a--) {
+    while ((money - keys[a]) > 0) {
+      hasil.push(keys[a])
+      money = money - keys[a]
     }
+    console.log(a, money, hasil)
   }
-  var hasilAkhir = []
-  for (var d = 0; d < hasil.length; d++) {
-    for (var e = 0; e < hasil[d].length; e++) {
-      hasilAkhir.push(hasil[d][e])
-    }
-  }
-  return hasilAkhir
+  return hasil
 }
 
 // Drive code
